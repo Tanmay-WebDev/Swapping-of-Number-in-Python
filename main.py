bee6f9794d6f00447af4ca7b54de3478d3a3bd19
DeveloperTanmay <umas81261@gmail.com>
2025-02-10 19:28:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# Swapping of Number in Python
+
+x = input("Enter the Value of x :")
+y = input("Enter the Value of y :")
+
+z = x
+x = y
+y = z
+
+print("The value of x after swapping :", x)
+print("The value of y after swapping :", y)
